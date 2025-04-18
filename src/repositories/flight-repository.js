51e8059bb13954024bd783523031daf4f2084465
Jay <jay.prajapati0204@gmail.com>
2025-04-18 23:10:33 +0530

    fixed bug in flight update

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -68,7 +68,7 @@ class FlightRepository extends CrudRepository {
 
     const flight = await Flight.findByPk(flightId);
 
-    if (parseInt(dec)) {
+    if (+dec) {
       await flight.decrement("totalSeats", { by: seats });
     } else {
       await flight.increment("totalSeats", { by: seats });
